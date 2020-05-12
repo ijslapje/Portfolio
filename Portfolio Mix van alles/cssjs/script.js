7e8eb924e7b87dbc1e944dac1cee302bbfa043c7
IJslap <45030475+ijslapje@users.noreply.github.com>
2020-05-12 21:39:53 +0200

    LAYOUT FOR CONTENT (nu nog alleen content 0_0)

--- Portfolio Mix van alles/cssjs/script.js
+++ Portfolio Mix van alles/cssjs/script.js
@@ -8,28 +8,15 @@ window.onload = function () {
         },
         mousewheel: true,
     });*/
-
     var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        centeredSlides: true,
+        effect: 'flip',
+        grabCursor: true,
         pagination: {
           el: '.swiper-pagination',
-          clickable: true,
         },
-        breakpoints: {
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-            },
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 50,
-            },
-        }
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
       });
 }
